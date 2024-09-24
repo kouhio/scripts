f185bd3a0559f4f3b927a2093c7293d120fcbdbc
Herra Kouhio <kouhio@gmail.com>
2024-09-24 21:32:23 +0300

    eachDir - handled all firectories now (also current)

--- eachDir.sh
+++ eachDir.sh
@@ -2,6 +2,9 @@
 
 [ -z "$1" ] && echo "No command given" && exit 1
 
+SUB=0
+[ "$1" == "sub" ] && SUB=1 && shift
+
 startSize=$(df --output=avail "$PWD" | sed '1d;s/[^0-9]//g')
 startTime=$(date +%s)
 startPath="$PWD"
@@ -31,19 +34,20 @@ set_interrupt () {
 
 trap set_interrupt SIGINT SIGTERM
 
-for d in */ ; do
-    error=0
+if [ "$SUB" -eq "0" ]; then
+    mapfile -t DIR_LIST <<< "$(find -type d | sort)"
+else
+    mapfile -t DIR_LIST <<< "$(find -type d -maxdepth 1 | sort)"
+fi
+
+D_MAX="${#DIR_LIST[@]}"
+D_CNT=0
+
+for d in "${DIR_LIST[@]}"; do
+    D_CNT="$((D_CNT + 1))"
     cd "$d" || continue
-    error=$?
-
-    if [ "$error" == "0" ]; then
-        echo -en "\nRunning in $d\n"
-        #source "$@"
-        "$@"
-    else
-        echo -en "\nFailed to enter $d\n"
-    fi
-
+    printf "\n[%0${#D_MAX}s/%${#D_MAX}s] Running in %s\n" "${D_CNT}" "${D_MAX}" "$d"
+    "${@}"
     cd "$startPath" || continue
 done
 
