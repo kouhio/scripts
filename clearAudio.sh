1cc4de206a16b16340869aefca2bac735b3ed52a
Herra Kouhio <kouhio@gmail.com>
2025-10-03 18:02:36 +0300

    Updated clearAudio to handle precise values

--- clearAudio.sh
+++ clearAudio.sh
@@ -15,28 +15,33 @@ COUNT=1
 # Print out help
 ################################################################
 help () {
-    echo "Audio portion removal application"
-    echo "Usage inputs:"
-    echo "1 - Target filetypes (mp4 etc)"
-    echo "2 - Source audio / video clip to remove from target files"
-    echo "3 - possible value in seconds to remove from the end of the video, if set as something else than 0"
-    echo "4 - if set, will write removal timeframes to a individualPack.sh handler instead of immediate action by packAll.sh"
-    echo "5 - If set, will erase everything from beginning to the end of the source audio"
+    printf "Audio portion removal application\n"
+    printf "Usage inputs:\n"
+    printf "1 - Target filetypes (mp4 etc)\n"
+    printf "<filepath>      - Any file given is used as a search source, other than the first input"
+    printf "beg=<seconds>   - Time to remove from the beginning, if the file first comparison file is not from the beginning\n"
+    printf "end=<seconds>   - Time to remove from the end, if last given file is note at the end\n"
+    printf "individual      - will write setup to do.shi, otherwise will use packAll.sh to strip the audio portion\n"
     exit 1
 }
 
 [ -z "$1" ] && lib C r "No input filetype given!\n" && help
 [ ! -f "$2" ] && lib C r "audio input file '$2' incorrect!\n" && help
-[ -n "$3" ] && CHECKER=$(lib v n "$3") && [ "$CHECKER" -ne "1" ] && lib C r "Input type '$3' invalid time value\n" && help
+#[ -n "$3" ] && CHECKER=$(lib v n "$3") && [ "$CHECKER" -ne "1" ] && lib C r "Input type '$3' invalid time value\n" && help
 
 ################################################################
 # add mp3s to database
 ################################################################
-AUDIO_LENGTH=$(mediainfo '--Inform=Audio;%Duration%' "$2")
-AUDIO_LENGTH=$((AUDIO_LENGTH / 1000))
-AUDIO_TENTH=$((AUDIO_LENGTH / 10))
-python3 ~/dev/audfprint/audfprint.py new --dbase tembase "$2" >/dev/null 2>&1
-echo "Audio comparison database from '$2', len:${AUDIO_LENGTH}s"
+createDatabase() {
+    AUDIO_LENGTH=$(mediainfo '--Inform=Audio;%Duration%' "$1")
+    AUDIO_LENGTH=$((AUDIO_LENGTH / 1000))
+    DATABASES+=("tembase_${LOOPCNT}")
+    DATABASELEN+=("${AUDIO_LENGTH}")
+    printf "Trying to create database from %s\n" "$1"
+    [ -f "tembase_${LOOPCNT}" ] && rm "tembase_${LOOPCNT}"
+    python3 ~/dev/audfprint/audfprint.py new --dbase "tembase_${LOOPCNT}" "$1" >/dev/null 2>&1
+    printf "Created audio comparison database from '%s', len:%ss\n" "${1}" "${AUDIO_LENGTH}"
+}
 
 ################################################################
 # Rewrite the do.sh file
@@ -46,10 +51,10 @@ writeOutput () {
     WRITE="$1"
     while IFS='' read -r line || [[ -n "$line" ]]; do
         if [ -z "$WRITE" ]; then
-            echo "$line" >> tempfile.txt
+            printf "$line\n" >> tempfile.txt
             [[ $line =~ "#BEGIN" ]]&& return 0
         else
-            [[ $line =~ "#END" ]] && WRITE="" && echo "$line" >> tempfile.txt
+            [[ $line =~ "#END" ]] && WRITE="" && printf "$line\n" >> tempfile.txt
         fi
     done < "do.sh"
 
@@ -63,8 +68,10 @@ writeOutput () {
 ################################################################
 set_int () {
     shopt -u nocaseglob
-    [ -f "tembase" ] && rm tembase
-    [ -z "$1" ] && echo " Interrupted at file $CURRCNT"
+    for i in "${DATABASES[@]}"; do
+        [ -f "${i}" ] && rm "${i}"
+    done
+    [ -z "$1" ] && printf " Interrupted at file %s\n" "$CURRCNT"
 
     ENDSIZE=$(df --output=avail "$PWD" | sed '1d;s/[^0-9]//g')
     TOTALSIZE=$((ENDSIZE - STARTSIZE))
@@ -91,10 +98,89 @@ endtime () {
 # 1 - Time in seconds
 #############################################################################
 calcTime () {
-    if [ "$1" -lt "60" ]; then TIME="$(date -d@"${1}" -u +%S)"
-    elif [ "$1" -lt "3600" ]; then TIME="$(date -d@"${1}" -u +%M:%S)";
-    else TIME="$(date -d@"${1}" -u +%T)"; fi
+    ADDTIME=""
+    ADDTIME="${1##*.}"
+    NORMTIME="${1%.*}"
+    if [ "$NORMTIME" -lt "60" ]; then TIME="$(date -d@"${NORMTIME}" -u +%S)"
+    elif [ "$NORMTIME" -lt "3600" ]; then TIME="$(date -d@"${NORMTIME}" -u +%M:%S)";
+    else TIME="$(date -d@"${NORMTIME}" -u +%T)"; fi
     [ "$TIME" == "00" ] && TIME="0"
+    [ -n "${ADDTIME}" ] && TIME+=".${ADDTIME}"
+}
+
+#############################################################################
+# Seek comparison audio from file
+# 1 - filename
+# 2 - database name
+#############################################################################
+seekAudio() {
+    printf "$(date +%T): Seeking audio from %-50s %8s\n" "${1:0:50}" "$(date -d@${VIDEO_LENGTH} -u +%T)"
+    I_OUTPUT=""
+    I_OUTPUT=$(python3 ~/dev/audfprint/audfprint.py match --dbase "${2}" "$1" --find-time-range)
+    AUDIO_LENGTH="${DATABASELEN[DBASE_POS]}"
+    ITEM=""
+
+    mapfile -t data_array < <(printf "%s" "$I_OUTPUT")
+
+    for index in "${!data_array[@]}"; do
+        if [[ "${data_array[index]}" =~ "Matched" ]]; then
+            ITEM="${data_array[index]}"
+            break
+        fi
+    done
+
+    [ -z "${ITEM}" ] && printf "Nothing found!\n%s\n" "${I_OUTPUT}" && return
+
+    mapfile -t -d " " data_array < <(printf "%s" "$ITEM")
+    item_array=()
+    for g in "${data_array[@]}"; do
+        [ -z "${g}" ] && continue
+        item_array+=("${g}")
+    done
+
+    I_LENGTH=""; I_START=""; I_COMPLEN=""
+
+    for i in "${!item_array[@]}"; do
+        if [ "${item_array[i]}" == "Matched" ]; then I_LENGTH="${item_array[i + 1]}"
+        elif [ "${item_array[i]}" == "starting" ] && [ "${item_array[i + 1]}" == "at" ]; then I_START="${item_array[i + 2]}"
+        elif [ "${item_array[i]}" == "to" ] && [ "${item_array[i + 1]}" == "time" ]; then I_COMPLEN="${item_array[i + 2]}"; fi
+
+        ((i++))
+    done
+
+    if [ -z "$I_LENGTH" ] || [ -z "$I_START" ] || [ -z "$I_COMPLEN" ]; then
+        lib C y "-> Skipping not found len:$I_LENGTH start:$I_START complen:$I_COMPLEN "
+        endtime
+        return
+    fi
+
+    TIME_CORRECTION=0
+    NEWSTART=$(bc <<< "scale=2;($I_START - $I_COMPLEN)")
+    if [ "${NEWSTART%.*}" == "-" ]; then NEWSTART=0
+    elif [ "${NEWSTART%.*}" -le "0" ]; then NEWSTART=$(bc <<< "scale=2;($AUDIO_LENGTH + $NEWSTART)"); fi
+
+    # Setup possible end value trimming
+    TIMECUT=0
+    [ -n "$ENDCLEAN" ] && [ "$ENDCLEAN" != "0" ] && TIMECUT=$((VIDEO_LENGTH - ENDCLEAN))
+
+    # reformat time values for later use
+    calcTime "$NEWSTART"
+    STARTHANDLE="$TIME"
+    #LASTSTART="$NEWSTART"
+
+    ENDSTART=$(bc <<< "scale=2;($NEWSTART + $AUDIO_LENGTH)")
+    calcTime "$ENDSTART"
+    ENDHANDLE="$TIME"
+
+    #calcTime "$TIMECUT"
+    #ENDHANDLE2="$TIME"
+    ENDPOS=$(bc <<< "scale=2;($VIDEO_LENGTH - $ENDSTART)")
+
+    if [ "${BEGCLEAN}" -ge "${STARTHANDLE%.*}" ] && [ "${DBASE_POS}" -eq "0" ]; then CUTARRAY+=("${ENDSTART}")
+    elif [ "$((DBASE_POS - 1))" -eq "${#DATABASELEN[@]}" ] && [ "${ENDCLEAN}" -gt "${ENDPOS%.*}" ]; then CUTARRAY+=("${ENDSTART}")
+    elif [ "$((DBASE_POS - 1))" -eq "${#DATABASELEN[@]}" ]; then CUTARRAY+=("${ENDSTART}" "0")
+    elif [ "${DBASE_POS}" -eq "0" ]; then CUTARRAY+=("0" "${STARTHANDLE}" "${ENDSTART}")
+    else CUTARRAY+=("${STARTHANDLE}" "${ENDSTART}");  fi
 }
 
 #############################################################################
@@ -108,114 +194,33 @@ calcTime () {
 get_info_and_cut () {
     STARTTIME=$(date +%s)
     VIDEO_LENGTH=$(mediainfo '--Inform=Video;%Duration%' "$1")
-    AUDIO_MIDDLE=$((VIDEO_LENGTH / 2))
-    [ -z "$AUDIO_LENGTH" ] && lib C r "incorrect audio $3 - len:'$AUDIO_LENGTH'\n" && exit 1
-    OUTPUTFILE="$2"
-    [ "$2" == "do.sh" ] && OUTPUTFILE="tempfile.txt"
+    [ -z "$VIDEO_LENGTH" ] && lib C r "incorrect video $1 - len:'$VIDEO_LENGTH'\n" && return
+    [ "${INDIVIDUAL}" -gt "0" ] && OUTPUTFILE="tempfile.txt"
 
-    VIDEO_LENGTH=$((VIDEO_LENGTH / 1000))
-    printf "$(date +%T): Seeking audio from %-50s %8s " "${1:0:50}" "$(date -d@${VIDEO_LENGTH} -u +%T)"
-    I_OUTPUT=""
-    I_OUTPUT=$(python3 ~/dev/audfprint/audfprint.py match --dbase tembase "$1" --find-time-range)
+    VIDEO_LENGTH=$((VIDEO_LENGTH / 1000)); CUTARRAY=(); DBASE_POS=0
 
-    NEW_LINE=$'\x0A';
-    mapfile -t -d "$NEW_LINE" data_array < <(printf "%s" "$I_OUTPUT")
-
-    for index in "${!data_array[@]}"; do
-        if [[ "${data_array[index]}" =~ "Matched" ]]; then
-            ITEM="${data_array[index]}"
-            break
-        fi
+    for j in "${DATABASES[@]}"; do
+        seekAudio "${1}" "${j}"
+        ((DBASE_POS++))
     done
 
-    mapfile -t -d "$NEW_LINE" data_array < <(printf "%s" "$ITEM")
-    I_LENGTH=""
-    I_START=""
-    I_COMPLEN=""
-
-    for i in "${!data_array[@]}"; do
-        if [ "${data_array[i]}" == "Matched" ]; then
-            I_LENGTH="${data_array[i + 1]}"
-        elif [ "${data_array[i]}" == "starting" ] && [ "${data_array[i + 1]}" == "at" ]; then
-            I_START="${data_array[i + 2]}"
-        elif [ "${data_array[i]}" == "to" ] && [ "${data_array[i + 1]}" == "time" ]; then
-            I_COMPLEN="${data_array[i + 2]}"
-        fi
-
-        i=$((i + 1))
-    done
-
-    if [ -z "$I_LENGTH" ] || [ -z "$I_START" ] || [ -z "$I_COMPLEN" ]; then
-        lib C y "-> Skipping not found len:$I_LENGTH start:$I_START complen:$I_COMPLEN "
-        endtime
-        return 0
-    fi
-
-    I_LENGTH="${I_LENGTH%.*}"
-    if [ "$I_LENGTH" -lt "$AUDIO_TENTH" ]; then
-        lib C y "-> Skipping too short len:${I_LENGTH}/${AUDIO_TENTH} start:$I_START complen:$I_COMPLEN "
-        endtime
-        return 0
-    fi
-
-    TIME_CORRECTION=0
-    NEWSTART=$(bc <<< "scale=0;($I_START - $I_COMPLEN)")
-    #ORIGSTART="${NEWSTART##.*}"
-    NEWSTART="${NEWSTART%.*}"
-    if [ "$NEWSTART" == "-" ]; then
-        NEWSTART=0
-    elif [ "$NEWSTART" -lt "10" ]; then
-        TIME_CORRECTION=$NEWSTART
-        NEWSTART=0
-    elif [ "$NEWSTART" -lt "0" ]; then
-        #NEWSTART=$((AUDIO_LENGTH - 1 + NEWSTART))
-        NEWSTART=$((AUDIO_LENGTH + NEWSTART))
-    else
-        NEWSTART=$((NEWSTART + 1))
-        #NEWLEN=$((AUDIO_LENGTH - 1))
-    fi
-
-    if [ "$NEWSTART" -ge "$AUDIO_MIDDLE" ]; then
-        lib C y "-> Skipping too far in the video $NEWSTART / $AUDIO_MIDDLE \n"
-        endtime
-        return 0
-    fi
-
-    I_START="${I_START%.*}"
-    CUTSTR=""
-
-    # Setup possible end value trimming
-    TIMECUT=0
-    [ -n "$4" ] && [ "$4" != "0" ] && TIMECUT=$((VIDEO_LENGTH - $4))
-
-    # reformat time values for later use
-    calcTime "$NEWSTART"
-    STARTHANDLE="$TIME"
-    #LASTSTART="$NEWSTART"
-
-    ENDSTART=$((NEWSTART + AUDIO_LENGTH))
-    calcTime "$ENDSTART"
-    ENDHANDLE="$TIME"
-
-    calcTime "$TIMECUT"
-    ENDHANDLE2="$TIME"
-
+    echo "DEBUG TODO: create cutter string here from ${CUTARRAY[*]}" && exit
     # Since no output file is given, remove the found timeframe immediately
-    if [ -z "$2" ]; then
+    if [ "${INDIVIDUAL}" -eq "0" ]; then
         error=0
 
         # Start is at the beginning, skip too short intro alltogether
-        if [ "$NEWSTART" -eq "0" ]; then
+        if [ "${NEWSTART%.*}" -eq "0" ]; then
             lib C y "-> Removing front:$((AUDIO_LENGTH + TIME_CORRECTION))s"
             [ -n "$4" ] && [ "$4" != "0" ] && lib C y " end:${4}s"
-            packAll.sh "$1" "quit" "c=$((AUDIO_LENGTH + TIME_CORRECTION))-${TIMECUT},D" >/dev/null 2>&1
+            echo "packAll.sh \"$1\" \"quit\" \"c=$((AUDIO_LENGTH + TIME_CORRECTION))-${TIMECUT},D\" >/dev/null 2>&1"
             error=$?
 
         # Comparison audio in the middle of the video, remove it from there
         else
-            printf "${Y}-> removing %8s-%-8s" "${STARTHANDLE}" "${ENDHANDLE}"
+            printf -- "-> removing %8s-%-8s" "${STARTHANDLE}" "${ENDHANDLE}"
             [ "$ENDHANDLE2" != "0" ] && lib C y " trim to:%-8s$" "${ENDHANDLE2}"
-            packAll.sh "${1}" "quit" C=0-${STARTHANDLE},${ENDHANDLE}-${ENDHANDLE2},D >/dev/null 2>&1
+            echo "packAll.sh \"${1}\" \"quit\" C=0-${STARTHANDLE},${ENDHANDLE}-${ENDHANDLE2},D >/dev/null 2>&1"
             error=$?
         fi
 
@@ -231,22 +236,22 @@ get_info_and_cut () {
 
     # Output file has been given, write removal timeframes to given file in the format of individualPack.sh
     else
-        lib C g "found start:$STARTHANDLE end:${ENDHANDLE} " && [ -n "$4" ] && [ "$4" != "0" ] && echo -en "trim:${ENDHANDLE2} "
+        lib C g "found start:$STARTHANDLE end:${ENDHANDLE} " && [ -n "$4" ] && [ "$4" != "0" ] && printf "trim:${ENDHANDLE2} "
 
         if [ -n "$5" ]; then
             CUTSTR="b=$ENDHANDLE"
             [ -n "$4" ] && [ "$4" != "0" ] && CUTSTR+=" e=${4}"
-            echo "PACK \"${1}\" $CUTSTR" >> "$OUTPUTFILE"
+            printf "PACK \"${1}\" $CUTSTR\n" >> "$OUTPUTFILE"
 
         elif [ "$NEWSTART" -eq "0" ]; then
             CUTSTR="b=$AUDIO_LENGTH"
             [ -n "$4" ] && [ "$4" != "0" ] && CUTSTR+=" e=${4}"
-            echo "PACK \"${1}\" $CUTSTR" >> "$OUTPUTFILE"
+            printf "PACK \"${1}\" $CUTSTR\n" >> "$OUTPUTFILE"
 
         else
-            echo "PACK \"${1}\" C=0-$STARTHANDLE,${ENDHANDLE}-${ENDHANDLE2},D" >> "$OUTPUTFILE"
+            printf "PACK \"${1}\" C=0-$STARTHANDLE,${ENDHANDLE}-${ENDHANDLE2},D\n" >> "$OUTPUTFILE"
         fi
-        [ "$COUNT" -ge "10" ] && COUNT=0 && echo -en "\n" >> "$OUTPUTFILE"
+        [ "$COUNT" -ge "10" ] && COUNT=0 && printf "\n" >> "$OUTPUTFILE"
         COUNT=$((COUNT + 1))
     fi
 
@@ -257,24 +262,33 @@ trap set_int SIGINT SIGTERM
 
 shopt -s nocaseglob
 
-FILECNT=$(find . -maxdepth 1 -name "*${1}" |wc -l)
 CURRCNT=1
-[ -f "$3" ] && rm "$3"
-[ ! -f "$4" ] && [ "$4" == "do.sh" ] && individualPack.sh "$1"
+LOOPCNT=0
+ENDCLEAN=""
+BEGCLEAN="0"
+INDIVIDUAL=0
+DATABASES=()
+DATABASELEN=()
+FILENAME="${1}"
+FILECNT=$(find . -maxdepth 1 -name "*${1}" |wc -l)
+shift
+
+for i in "${@}"; do
+    if [ -f "${i}" ]; then createDatabase "${i}"
+    elif [[ "${i}" == "end="* ]]; then ENDCLEAN="${i##*=}"
+    elif [[ "${i}" == "beg="* ]]; then BEGCLEAN="${i##*=}"
+    elif [[ "${i}" == "individual" ]]; then INDIVIDUAL=1 && individualPack.sh "${FILENAME}"; fi
+
+    ((LOOPCNT++))
+done
+
 [ -f "tempfile.txt" ] && rm "tempfile.txt"
 
-[ "$4" == "do.sh" ] && writeOutput
-
-for f in *".${1}"; do
+for f in *".${FILENAME}"; do
     printf "%03d/%03d " "$CURRCNT" "$FILECNT"
-    get_info_and_cut "$f" "$4" "$2" "$3" "$5"
+    get_info_and_cut "$f"
     CURRCNT=$((CURRCNT + 1))
 done
 
-[ "$4" == "do.sh" ] && writeOutput "1" && chmod 777 do.sh
-
 shopt -u nocaseglob
-
-rename "s/_01//" ./*"${1}"
-
 set_int 1
